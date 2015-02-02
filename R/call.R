d760aa93830c01ad5910be6b8738c8e67684b294
Jason Cohen <JasonCEC@Gastrograph.com>
2015-02-01 22:07:58 -0500

    Added Statless Calling
    woot! awesome, not writing to ephemeral disk anymore!

--- R/call.R
+++ R/call.R
@@ -26,6 +26,50 @@ twilio_call <- function(accountSID, auth_token, xmlURL, from, to) {
    .post(url, body, accountSID, auth_token)
 } 
 
+
+#' Send an outbound call without writing the file to disk
+#'
+#' this function sends an outbound call using a twillio twimlit,
+#' allowing statless applications calling functionality
+#'
+#' @param accountSID Your account ID
+#'
+#' @param auth_token Your secret token
+#'
+#' @param message the message to transcribe to speach. By default this will be spoken in by a female
+#' in british english and repeated 3 times. \emph{Either message or Twxml must be null!
+#' you cannot send a call with both parameters set}
+#'
+#' @param TwXML Any properly formated XML acceptable by Twillio
+#'
+#' @param from The phonenumber to call. Check Your Twilio account for the number.
+#' Make sure to include the international number.
+#'
+#' @param to The phone number to call. Make sure to include the international number.
+#'
+#' @export
+#'
+twilio_stateless_call <- function(accountSID, auth_token, message=NULL, TwXML=NULL, from, to) {
+  ## Initial Error Checking
+  if(is.null(message) & is.null(TwXML)) stop("Either message or TwXML must be set!")
+  if(all(!is.null(message), !is.null(TwXML))) stop("Only message or TwXML can be set at one time!")
+
+  ## Base URL for POST
+  url <- paste0('https://api.twilio.com/2010-04-01/Accounts/', accountSID, '/Calls.json')
+
+  if(is.null(TwXML)) {
+    TwXML <- URLencode(paste0("<Response><Say voice='alice' language='en-gb' loop='3'>", message,
+                              "</Say></Response>"))
+  } else {
+    ## Encode XML structure as a url charecter vector
+    TwXML <- URLencode(as(TwXML, "character"))
+  }
+
+  body <- list(Url=paste0("http://twimlets.com/echo?Twiml=", TwXML), From=from, Method="GET", To=to)
+  .post(url, body, accountSID, auth_token)
+}
+
+
 .post <- function(url, body, accountSID, auth_token) {
   res <- POST(url = url, user_agent="RTwilio", 
               config=authenticate(accountSID, auth_token, "basic"), body=body)
