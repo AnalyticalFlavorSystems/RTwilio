40382fe54a0c6a26050e978c7beb08da149e764e
Jason Cohen <JasonCEC@Gastrograph.com>
2015-02-07 18:54:15 -0500

    Created a RTwillio.R file for dependency managment
    @EvanDev you need to pull my changes and re-build the package using devtools!

---
+++ R/RTwilio.R
@@ -0,0 +1,9 @@
+#' Use Twillio from R 
+#'
+#' a set of functions and convienence wrappers to make life building twillio apps a little easier 
+#'
+#' @import XML httr RCurl
+#'
+#' @docType package
+#' @name RTwilio
+NULL
